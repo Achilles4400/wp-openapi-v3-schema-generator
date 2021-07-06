797af8e070f15c1f9f8406ec80ae4f9d2d53de5e
Vincent Bathellier <vincent.bathellier@cobeia.com>
2021-07-06 11:26:11 +0200

    update version of plugin

--- plugin.php
+++ plugin.php
@@ -1,25 +1,28 @@
 <?php
+
 /**
  * Plugin Name: wp-openapi-v3-schema-generator
  * Description: Cobeia Open API v3 generator for the WP REST API
  * Author: Vincent Bathellier
  * Author URI: 
- * Version: 2.0.12
+ * Version: 2.0.13
  * Plugin URI: https://github.com/Achilles4400/wp-api-swaggerui.git
  * License: Cobeia
  */
 
-function swagger_rest_api_init() {
+function swagger_rest_api_init()
+{
 
-	if ( class_exists( 'WP_REST_Controller' )
-		&& ! class_exists( 'WP_REST_Swagger_Controller' ) ) {
-		require_once dirname( __FILE__ ) . '/lib/class-wp-rest-swagger-controller.php';
+	if (
+		class_exists('WP_REST_Controller')
+		&& !class_exists('WP_REST_Swagger_Controller')
+	) {
+		require_once dirname(__FILE__) . '/lib/class-wp-rest-swagger-controller.php';
 	}
 
 
 	$swagger_controller = new WP_REST_Swagger_Controller();
 	$swagger_controller->register_routes();
-
 }
 
-add_action( 'rest_api_init', 'swagger_rest_api_init', 11 );
+add_action('rest_api_init', 'swagger_rest_api_init', 11);
